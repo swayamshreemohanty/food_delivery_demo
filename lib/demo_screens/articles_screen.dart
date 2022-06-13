a78fba56415115333cf4b240da9344cef389b9f5
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-13 11:26:03 +0530

    logout button added

--- lib/demo_screens/articles_screen.dart
+++ lib/demo_screens/articles_screen.dart
@@ -1,12 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_delivery/authentication/logic/authentication/authentication_bloc.dart';
 
 class ArticlesScreen extends StatelessWidget {
   const ArticlesScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const Center(
-        child: Text("This is Article Screen"),
+  Widget build(BuildContext context) => Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("This is Article Screen"),
+            IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: const Text("Are you sure!"),
+                    content: const Text("You are about to logout!"),
+                    actions: [
+                      DialogActionButton(
+                        buttonName: 'No',
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      DialogActionButton(
+                        buttonName: 'Yes',
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          context.read<AuthenticationBloc>().add(LoggedOut());
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+              icon: const Icon(Icons.exit_to_app_rounded),
+            )
+          ],
+        ),
       );
+}
+
+class DialogActionButton extends StatelessWidget {
+  final String buttonName;
+  final void Function()? onPressed;
+  const DialogActionButton({
+    Key? key,
+    required this.buttonName,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.white),
+        foregroundColor: MaterialStateProperty.all(Colors.black),
+      ),
+      onPressed: onPressed,
+      child: Text(buttonName),
+    );
+  }
 }
