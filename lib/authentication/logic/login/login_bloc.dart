e565476f034de26c43fc4d6a77588acaf4e2a363
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-13 11:19:01 +0530

    error handler added

--- lib/authentication/logic/login/login_bloc.dart
+++ lib/authentication/logic/login/login_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:food_delivery/authentication/logic/authentication/authentication_bloc.dart';
-import 'package:food_delivery/authentication/model/token_model.dart';
 import 'package:food_delivery/authentication/repository/auth_repository.dart';
 part 'login_event.dart';
 part 'login_state.dart';
@@ -15,27 +15,25 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
         final userRepository = AuthenticationRepository();
         if (event is LoginButtonPressed) {
           emit(LoginLoading());
-
           try {
-            final response = await userRepository.authenticate(
-              userEmail: event.userEmail,
-              password: event.password,
-            );
-
-            authenticationBloc.add(
-              LoggedIn(tokenData: TokenData(token: response)),
-            );
+            await userRepository.authenticate(
+                userEmail: event.userEmail,
+                password: event.password,
+                isLogin: event.isLogin);
+            authenticationBloc.add(LoggedIn());
             emit(LoginInitial());
             return;
           } catch (error) {
-            emit(
-              const LoginFailure(rawError: "Error occured while authenticate"),
-            );
+            if (error is FirebaseException) {
+              emit(LoginFailure(
+                  rawError: error.message ?? "Something went wrong."));
+            } else {
+              emit(const LoginFailure(
+                  rawError: "Error occured while authenticate"));
+            }
           }
         }
       },
     );
   }
-
-
 }
