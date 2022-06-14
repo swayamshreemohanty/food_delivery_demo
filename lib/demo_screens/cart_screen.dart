07833101904d04e73ab153a785f35fb2d6e0a166
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-14 16:33:32 +0530

    Cart screen updated

--- lib/demo_screens/cart_screen.dart
+++ lib/demo_screens/cart_screen.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:food_delivery/bottom_bar_content/logic/go_to_home/go_to_home_cubit.dart';
+import 'package:flutter/material.dart';
 
 class CartScreen extends StatelessWidget {
   static const String routeName = '/cart_screen';
 
   const CartScreen({Key? key}) : super(key: key);
 
-  Future<bool> willPopScope(BuildContext context) async {
-    BlocProvider.of<GoToHomeCubit>(context).goToHomeHome();
-    //Return false to not to close the app when the back button will triggered.
-    return false;
-  }
-
   @override
-  Widget build(BuildContext context) => WillPopScope(
-        onWillPop: () => willPopScope(context),
-        child: const Center(
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+        ),
+        body: const Center(
           child: Text("This is Cart Screen"),
         ),
       );
