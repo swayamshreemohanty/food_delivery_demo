ee23855d9526675ae9417febfba79be5c65e001e
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-14 14:05:15 +0530

    carousel slider widget added

---
+++ lib/bottom_bar_content/home/widget/food_catalog_carousel.dart
@@ -0,0 +1,44 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery/bottom_bar_content/home/widget/food_catalog.dart';
+import 'package:food_delivery/constant/dummy_dart.dart';
+
+class FoodCatelogCarousel extends StatefulWidget {
+  final List<Food> foodlist;
+  const FoodCatelogCarousel({
+    Key? key,
+    required this.foodlist,
+  }) : super(key: key);
+
+  @override
+  State<FoodCatelogCarousel> createState() => _FoodCatelogCarouselState();
+}
+
+class _FoodCatelogCarouselState extends State<FoodCatelogCarousel> {
+  final CarouselController _controller = CarouselController();
+
+  @override
+  Widget build(BuildContext context) => CarouselSlider.builder(
+        itemCount: widget.foodlist.length,
+        carouselController: _controller,
+        itemBuilder: (_, index, realIndex) => Align(
+          alignment: const Alignment(-1, -1.0),
+          child: FoodCatalog(food: widget.foodlist[index]),
+        ),
+        options: CarouselOptions(
+          disableCenter: false,
+          viewportFraction: 0.6,
+          enlargeCenterPage: true,
+          height: MediaQuery.of(context).size.height,
+          enableInfiniteScroll: false,
+          initialPage: 0,
+          enlargeStrategy: CenterPageEnlargeStrategy.height,
+          // onPageChanged: onPageChanged,
+          // viewportFraction: 0.6,
+          // enableInfiniteScroll: false,
+          // enlargeCenterPage: true,
+
+          // onPageChanged: (index, reason) {},
+        ),
+      );
+}
