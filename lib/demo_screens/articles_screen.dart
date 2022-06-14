3bc80c18fbcb7c6b4273324267c765aab7ff517b
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-14 08:58:48 +0530

    logout removed

--- lib/demo_screens/articles_screen.dart
+++ lib/demo_screens/articles_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:food_delivery/authentication/logic/authentication/authentication_bloc.dart';
 import 'package:food_delivery/bottom_bar_content/logic/go_to_home/go_to_home_cubit.dart';
 
 class ArticlesScreen extends StatelessWidget {
@@ -15,62 +14,8 @@ class ArticlesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => WillPopScope(
         onWillPop: () => willPopScope(context),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("This is Article Screen"),
-              IconButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (ctx) => AlertDialog(
-                      title: const Text("Are you sure!"),
-                      content: const Text("You are about to logout!"),
-                      actions: [
-                        DialogActionButton(
-                          buttonName: 'No',
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        DialogActionButton(
-                          buttonName: 'Yes',
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                            context.read<AuthenticationBloc>().add(LoggedOut());
-                          },
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                icon: const Icon(Icons.exit_to_app_rounded),
-              )
-            ],
-          ),
+        child: const Center(
+          child: Text("This is Article Screen"),
         ),
       );
-}
-
-class DialogActionButton extends StatelessWidget {
-  final String buttonName;
-  final void Function()? onPressed;
-  const DialogActionButton({
-    Key? key,
-    required this.buttonName,
-    required this.onPressed,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return OutlinedButton(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(Colors.white),
-        foregroundColor: MaterialStateProperty.all(Colors.black),
-      ),
-      onPressed: onPressed,
-      child: Text(buttonName),
-    );
-  }
 }
