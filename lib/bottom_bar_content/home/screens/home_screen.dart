887bf8a59012466b1fc67a43f5f33d17d2b406e6
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-14 11:51:05 +0530

    ui updated

--- lib/bottom_bar_content/home/screens/home_screen.dart
+++ lib/bottom_bar_content/home/screens/home_screen.dart
@@ -1,10 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_delivery/bottom_bar_content/home/logic/food_management/foodmanagemet_bloc.dart';
+import 'package:food_delivery/bottom_bar_content/home/widget/app_drawer.dart';
+import 'package:food_delivery/bottom_bar_content/home/widget/food_category.dart';
+import 'package:food_delivery/bottom_bar_content/home/widget/search_bar.dart';
+import 'package:food_delivery/bottom_bar_content/home/widget/shadow_container.dart';
+import 'package:food_delivery/utility/firebase_current_user_data.dart';
+import 'package:food_delivery/utility/loading_indicator.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const Center(
-        child: Text("This is Home Screen"),
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldkey = new GlobalKey<ScaffoldState>();
+  @override
+  void initState() {
+    context.read<FoodmanagemetBloc>().add(FetchFoodsDetailsForHome());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        key: _scaffoldkey,
+        drawer: AppDrawer(
+          userDetails: FirebaseCurrentUserData().userDetails!,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 40.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.w),
+              child: Row(children: [
+                ShadowContainer(
+                  opacity: 0.2,
+                  offset: const Offset(0.8, 8),
+                  backgroundColor: Colors.white,
+                  child: IconButton(
+                      iconSize: 25.sp,
+                      onPressed: () {
+                        _scaffoldkey.currentState!.openDrawer();
+                      },
+                      icon: const Icon(
+                        Icons.menu,
+                      )),
+                ),
+              ]),
+            ),
+            SizedBox(height: 20.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.w),
+              child: const PageHeadingWidget(
+                text1: "Let's eat",
+                text2: "Quality food 😋",
+              ),
+            ),
+            SizedBox(height: 20.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w),
+              child: const SearchBarWithFilter(),
+            ),
+            SizedBox(height: 40.h),
+            BlocBuilder<FoodmanagemetBloc, FoodmanagemetState>(
+              builder: (context, state) {
+                if (state is FoodDataLoading) {
+                  return const LoadingIndicator();
+                } else if (state is FoodDataFetched) {
+                  return Padding(
+                    padding: EdgeInsets.only(left: 15.w),
+                    child: FoodCategory(foodCategory: state.foodCategory),
+                  );
+                } else {
+                  return const Center(
+                    child: Text('No Data found'),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       );
+}
+
+class PageHeadingWidget extends StatelessWidget {
+  final String text1;
+  final String text2;
+  final Color? textColor;
+  const PageHeadingWidget({
+    Key? key,
+    required this.text1,
+    required this.text2,
+    this.textColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // final deviceSize = MediaQuery.of(context).size;
+    final textStyle = GoogleFonts.poppins(
+      textStyle: TextStyle(
+        color: textColor ?? Colors.black,
+        fontWeight: FontWeight.bold,
+        fontSize: 31.sp,
+      ),
+    );
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 10.w,
+      ),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Text(text1, style: textStyle),
+        Text(text2, style: textStyle),
+      ]),
+    );
+  }
 }
