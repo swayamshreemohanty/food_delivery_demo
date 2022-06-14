1bed47c974d38fa92f518b17689f3677e74f6012
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-06-14 11:50:54 +0530

    shadow container added

---
+++ lib/bottom_bar_content/home/widget/shadow_container.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ShadowContainer extends StatelessWidget {
+  final Color? backgroundColor;
+  final Widget child;
+  final Offset? offset;
+  final double? opacity;
+  const ShadowContainer({
+    Key? key,
+    this.backgroundColor,
+    this.offset,
+    this.opacity,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(opacity ?? 0.5),
+            spreadRadius: 0.1.r,
+            blurRadius: 2.r,
+            offset: offset ?? const Offset(0, 0), // changes position of shadow
+          ),
+        ],
+        color: backgroundColor ?? Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(12.r),
+      ),
+      child: child,
+    );
+  }
+}
